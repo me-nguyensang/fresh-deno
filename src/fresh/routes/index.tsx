46c993ab6a4107e2dd801abe77591cc25026ac6f
me-nguyensang <nguyen.sang@marketenterprise.vn>
2022-09-28 14:10:55 +0700

    [main] Update source code

---
+++ src/fresh/routes/index.tsx
@@ -0,0 +1,16 @@
+import Counter from "../islands/Counter.tsx";
+
+export default function Home() {
+  return (
+    <div class="p-4 mx-auto max-w-screen-md">
+      <img
+        src="https://mevn-public.s3-ap-southeast-1.amazonaws.com/marketenterprise.vn/images/me_logo.png"
+        class="h-32"
+        alt="The MarketEnterprise logo"
+      />
+      <p class="my-6">
+        Welcome to MarketEnterprise Vietnam. <a href="https://www.marketenterprise.vn/" target="_blank">https://www.marketenterprise.vn/</a>
+      </p>
+    </div>
+  );
+}
